84a2ef0ba4ecb0fec07d451be1e643d08a6937dd
NadiaQN <quezada.nadia@gmail.com>
2017-11-21 12:00:20 -0300

    arreglos footer y js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,23 @@
 /*
  * Funcionalidad de tu producto
  */
- document.getElementsByClassName('tabcontent').style.display = 'none'
+//funcion navegacion por tabs
+//con la funcion load mantengo una pestaña predeterminada
+window.addEventListener("load",function(){
+document.getElementById('estudent').style.display="none";
+document.getElementById('teacher').style.display="none";
+});
+//con la funcion openpage se muestra el contenido de cada pestaña
+var openPage = function(id){
+	var tabs = document.getElementsByClassName('tabcontent');
+	for(i=0; i<tabs.length; i++){
+		tabs[i].style.display="none";
+	}
+	document.getElementById(id).style.display = "";
+};
+//fin navegación por tabs
+
+
+
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
